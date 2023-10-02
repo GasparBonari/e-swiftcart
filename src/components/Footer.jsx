7efed0f42636eee911b9d5c0f552cef480200642
Gaspar Bonari <gasparbonari@gmail.com>
2023-10-02 14:25:59 +0200

    .

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../styles/footer.css'
+import Logo from '../images/logo.png';
+
+function Footer()
+{
+  return (
+    <footer className="footer">
+      <div className="footer-row">
+        <div className="logo">
+            <img href="/home" src={Logo}></img>
+          <h2>Everything you want anytime, anywhere.</h2>
+        </div>
+      </div>
+      <div className="footer-row">
+        <nav className="nav-links">
+            <a href="/">Home</a>
+            <a href="/products">Products</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </nav>
+      </div>
+      <div className="footer-row">
+        <div className="subscribe">
+          <h3>Subscribe to Our Newsletter</h3>
+          <div className="subscribe-form">
+            <input type="email" placeholder="Your Email..." />
+            <button>Subscribe</button>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import Home from './pages/Home.jsx'
 import Navbar from './components/NavBar.jsx'
+import Footer from './components/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar/>
     <Home/>
+    <Footer/>
   </React.StrictMode>,
 )
