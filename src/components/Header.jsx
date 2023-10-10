0e3fac67daa9e46200b97d6467c4ba94d5d62fc4
Gaspar Bonari <gasparbonari@gmail.com>
2023-10-10 13:34:46 +0200

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,28 @@ function Header()
 {
   return (
     <header className="header-container background-fixed">
+
+<div id="background-wrap">
+    <div class="x1">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x2">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x3">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x4">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x5">
+        <div class="cloud"></div>
+    </div>
+</div>
       <div className="header-content">
         <h1>SwiftCart</h1>
         <p className="header-p">The web to find any product you want</p>
