013b9d3be956fe4e7d360ed9ca9da89334fceb1b
Gaspar Bonari <89667053+GasparBonari@users.noreply.github.com>
2023-10-21 19:39:24 +0200

    .

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -5,28 +5,30 @@ import '../styles/shoppingCart.css';
 
 const ShoppingCart = ({ cartItems, toggleShoppingCart }) => {
   return (
-    <div className="shopping-cart-overlay">
-      <div className="shopping-cart">
-        <div className="cart-header">
-          <h2>Shopping Cart</h2>
-          <FontAwesomeIcon icon={faTimes} onClick={toggleShoppingCart} className="close-icon" />
-        </div>
-        <div className="cart-items">
-          {cartItems.length === 0 ? (
-            <p>Your cart is empty.</p>
-          ) : (
-            <ul>
-              {cartItems.map((item, index) => (
-                <li key={index}>
-                  <span>{item.title}</span>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div className="cart-footer">
-          {/* Add additional cart summary information if needed */}
-        </div>
+    <div className="shopping-cart">
+      <div className="cart-header">
+        <h2>Shopping Cart</h2>
+        <FontAwesomeIcon icon={faTimes} onClick={toggleShoppingCart} className="close-icon" />
+      </div>
+      <div className="cart-items">
+        {cartItems.length === 0 ? (
+          <p>Your cart is empty.</p>
+        ) : (
+          <ul>
+            {cartItems.map((item, index) => (
+              <li key={index}>
+                <div className="cart-item">
+                  <img src={item.images[0]} alt={item.title} className="cart-item-image" />
+                  <span className="cart-item-title">{item.title}</span>
+                  <span className="cart-item-price">${item.price}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="cart-footer">
+        {/* Add additional cart summary information if needed */}
       </div>
     </div>
   );
