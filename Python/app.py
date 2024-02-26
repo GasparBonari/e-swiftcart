08926a5e4f474e6e1ed34a1a98d44274098f0793
GasparBonari <gasparbonari@gmail.com>
2024-02-26 16:09:05 +0100

    connecting back end with front end

--- Python/app.py
+++ Python/app.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins="*")  # Allow requests from any origin (for testing only)
-socketio = SocketIO(app)
+CORS(app, origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('message_from_frontend')
 def handle_message(message):
     print('Message from frontend:', message)
-    # Process the message as needed
-    # You can emit a response back to the frontend if necessary
     socketio.emit('message_from_backend', 'Message received on the backend')
 
 if __name__ == '__main__':
